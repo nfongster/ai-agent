96a35309af173b561b7d84556c421898b46c0d69
Nick <nickfong@2017-041.rz.local>
2025-06-18 08:48:28 -0700

    Add verbose flag

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ client = genai.Client(api_key=api_key)
 args = sys.argv
 if len(args) < 2:
     sys.exit("Too few args were provided.  Please provide a prompt, in quotes, as an argument.")
-prompt = args[1]
+user_prompt = args[1]
+verbose = len(args) > 2 and args[2] == "--verbose"
+if verbose:
+    print(f"User prompt: {user_prompt}")
 
 messages = [
     types.Content(role="User", parts=[types.Part(text=user_prompt)]),
@@ -24,5 +27,7 @@ response = client.models.generate_content(
 print(f"Response:\n\n{response.text}")
 
 prompt_tokens, response_tokens = response.usage_metadata.prompt_token_count, response.usage_metadata.candidates_token_count
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {response_tokens}")
+
+if verbose:
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
