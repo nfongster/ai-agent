2507dbaf5917364b83a3786b8a1729522f032d37
Nick <nickfong@2017-041.rz.local>
2025-06-19 10:47:46 -0700

    Add write_file function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -44,3 +44,22 @@ def get_file_content(working_directory, file_path):
 
     except Exception as e:
         print(f'Error: {e}')
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        working_dir_abs_path = os.path.abspath(working_directory)
+        file_abs_path = os.path.abspath(os.path.join(working_dir_abs_path, file_path))
+
+        if not file_abs_path.startswith(working_dir_abs_path):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(file_abs_path):
+            os.makedirs(os.path.dirname(file_abs_path), exist_ok=True)
+        
+        with open(file_abs_path, 'w') as f:
+            f.write(content)
+        
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        
+    except Exception as e:
+        print(f'Error: {e}')
