79ac13053ce147be74e4618e316b5c8ab62b5691
Nick <nickfong@2017-041.rz.local>
2025-06-19 11:21:39 -0700

    add system prompt

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ messages = [
     types.Content(role="User", parts=[types.Part(text=user_prompt)]),
 ]
 
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
     contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 print(f"Response:\n\n{response.text}")
