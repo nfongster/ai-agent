f5a3d82ea22f4cb81a1764ca67139e397f1f108b
Nick <nickfong@2017-041.rz.local>
2025-06-19 10:27:58 -0700

    Add get_file_content function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import os
 
+# Max number of characters to read from a file
+MAX_CHARS = 10000
+
+
 def get_files_info(working_directory, directory=None):
     try:
         working_dir_path = os.path.abspath(working_directory)
@@ -18,5 +22,25 @@ is_dir={os.path.isdir(os.path.join(sub_dir_path, item))}'
             for item in os.listdir(sub_dir_path)]
         )
     
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_dir_abs_path = os.path.abspath(working_directory)
+        file_abs_path = os.path.abspath(os.path.join(working_dir_abs_path, file_path))
+
+        if not os.path.isfile(file_abs_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        if not file_abs_path.startswith(working_dir_abs_path):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        with open(file_abs_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if len(f.read()) > MAX_CHARS:
+                file_content_string += f'...File "{file_abs_path}" truncated at 10000 characters'
+            return file_content_string
+
     except Exception as e:
         print(f'Error: {e}')
